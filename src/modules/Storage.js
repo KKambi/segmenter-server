c81df56afa5054879e3fbcdaa17a136e7ea23982
KKambi <37494867+KKambi@users.noreply.github.com>
2019-11-14 18:13:23 +0900

    [Add] 스트림 데이터 생성을 위한 모듈 구현
    - 스트림 데이터 생성을 위해 모듈들을 구현

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -32,18 +32,26 @@ const Storage = {
     console.log(`${name} 업로드 완료!`);
   },
 
-  downloadVideo: async (name) => {
-    const localFilePath = path.join(__dirname, '/tmp.mp4')
-    let outStream = fs.createWriteStream(localFilePath);
-    let inStream = S3.getObject({
-        Bucket: process.env.BUCKET_NAME + "/thumbnails",
-        Key: name
-    }).createReadStream(); 
+  downloadVideo: async (dirName, videoName, bucketPath) => {
+    // 해당 비디오의 폴더가 존재하지 않으면 생성
+    const localDirPath = path.join(__dirname, dirName);
+    !fs.existsSync(localDirPath) && fs.mkdirSync(localDirPath)
 
+    // Storage부터 해당 비디오 다운로드 시작
+    let outStream = fs.createWriteStream(localDirPath + '/' + videoName);
+    let inStream = S3.getObject({
+        Bucket: bucketPath,
+        Key: videoName
+    }).createReadStream(); 
     inStream.pipe(outStream);
-    inStream.on('end', () => {
-      console.log(`${name} 다운로드 완료!`);
-    })
+
+    // 다운로드가 완료되면 메세지 출력!
+    await new Promise((resolve) => {
+      inStream.on('end', () => {
+        console.log(`${bucketPath} -> ${videoName} 다운로드 완료!`);
+        resolve();
+      })
+    });
   },
 }
 
--- src/modules/Transcoder.js
+++ src/modules/Transcoder.js
@@ -2,49 +2,49 @@ const fetch = require("node-fetch");
 require("dotenv").config();
 
 const Signature = require('./Signature');
+const Parser = require('./Parser');
 const Job = require('./Job');
 
+// API 요청 정보
+const URL = "https://vodtranscoder.apigw.ntruss.com/api/v2/jobs";
+const timestamp = Date.now();
+
+// API 요청을 위한 시그네처
+const signature = Signature.createSignature(
+  process.env.SECRET_KEY,
+  "POST",
+  "/api/v2/jobs",
+  String(timestamp),
+  process.env.ACCESS_KEY
+);
+
+// Transcoder 모듈
 const Transcoder = {
-  // API 요청 URL
-  URL: "https://vodtranscoder.apigw.ntruss.com/api/v2/jobs",
-  
-  // API 요청을 위한 시그네처
-  signature: Signature.createSignature(
-    process.env.SECRET_KEY,
-    "POST",
-    "/api/v2/jobs",
-    String(timestamp),
-    process.env.ACCESS_KEY
-  ),
-
-  requestJob: async (fileName, filePath) => {
-    // 요청 시의 타임스탬프
-    const timestamp = Date.now();
-
+  requestJob: async (fileName) => {
     // Job 정보설정
-    // name: "#2 4 Protection with PropTypes"
-    // path: "/videos/#2 4 Protection with PropTypes.mp4"
+    const name = Parser.removeExtension(fileName)
+    const path = Parser.createStoragePath(fileName);
     const job = Job.createJob(
-      fileName,
-      filePath,
+      name,
+      path,
       process.env.BUCKET_NAME
     );
 
     // Transcoder에 Job생성을 요청
-    const data = await fetch(this.URL, {
+    const data = await fetch(URL, {
       method: "POST",
       body: JSON.stringify(job),
       headers: {
         "Content-Type": "application/json",
         "x-ncp-iam-access-key": process.env.ACCESS_KEY,
         "x-ncp-apigw-api-key": process.env.API_KEY,
-        "x-ncp-apigw-signature-v2": this.signature,
-        "x-ncp-apigw-timestamp": timestamp
+        "x-ncp-apigw-signature-v2": signature,
+        "x-ncp-apigw-timestamp": timestamp,
       }
     });
     const result = await data.json();
-    console.log(result);
-  }
+    console.log(`${fileName}요청상태: ${result.error.message}`);
+  },
 }
 
 module.exports = Transcoder;
