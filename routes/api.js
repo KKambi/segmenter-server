77d91a824a7763a915f848b169524364e5a4470a
KKambi <37494867+KKambi@users.noreply.github.com>
2019-11-19 13:30:20 +0900

    Add: Job응답 받는 라우팅 구현
    - Job응답을 받기 위한 라우팅

--- routes/api.js
+++ routes/api.js
@@ -16,5 +16,9 @@ router.post('/segments', (req, res, next) => {
 });
 
 // Job 상태변경에 대한 알림을 받은 뒤, 개별항목에 대해 스트림 데이터 생성
+router.post('/segment', (req, res, next) => {
+  const { jobId, status } = req;
+  console.log(`${jobId}, ${status}`);
+});
 
 module.exports = router;
