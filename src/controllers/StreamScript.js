5f283db3da09378a9f948fd6c253fc3db9f70aa9
KKambi <37494867+KKambi@users.noreply.github.com>
2019-11-26 15:25:34 +0900

    Fix: insert query async/await try..catch

--- src/controllers/StreamScript.js
+++ src/controllers/StreamScript.js
@@ -104,25 +104,28 @@ const StreamScript = {
 
   insertURLtoDB: async files => {
     const inserts = [];
-    files.forEach(fileName => {
-      // TODO: adaptive bit streaming 어떻게?
-      const nameWithoutExt = Parser.removeExtension(fileName);
-      const URL = `${process.env.CDN_URL}/videos/${nameWithoutExt}/720p.mp4`;
-      const datetime = new Date().format("dd/MM/yyyy hh:mm TT");
-      inserts.push(
-        VideoModel.create({
-          name: nameWithoutExt,
-          category: "테스트", // TODO: 카테고리 변경
-          likes: 0,
-          reg_date: datetime,
-          thumbnail_img_url: null, // TODO: 썸네일 이미지
-          thumbnai_video_url: null,
-          streaming_url: URL
-        })
-      );
-    });
-
-    await Promise.all(inserts);
+    try {
+      files.forEach(fileName => {
+        // TODO: adaptive bit streaming 어떻게?
+        const nameWithoutExt = Parser.removeExtension(fileName);
+        const URL = `${process.env.CDN_URL}/videos/${nameWithoutExt}/720p.mp4`;
+        const datetime = new Date().format("dd/MM/yyyy hh:mm TT");
+        inserts.push(
+          VideoModel.create({
+            name: nameWithoutExt,
+            category: "테스트", // TODO: 카테고리 변경
+            likes: 0,
+            reg_date: datetime,
+            thumbnail_img_url: null, // TODO: 썸네일 이미지
+            thumbnai_video_url: null,
+            streaming_url: URL
+          })
+        );
+      });
+      await Promise.all(inserts);
+    } catch (err) {
+      console.log(`DB insert 에러:${err}`);
+    }
   }
 };
 
